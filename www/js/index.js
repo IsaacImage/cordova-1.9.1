13331b6753d83052c9613349c85ced6a321a9cb7
exson <isaac@imagead.net>
2013-10-17 11:03:27 +0100

    Remove receivedEvent from index.js

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
         //Check if database exist
         if (!db) {
             //Database does not exist ; create it
